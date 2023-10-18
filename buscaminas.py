20c83616263f30d68413a0e223ffdff7785a15a5
leogomez49 <jonasgomez08@gmail.com>
2023-10-18 15:22:55 -0300

    update code

--- buscaminas.py
+++ buscaminas.py
@@ -1,13 +1,20 @@
 from seting import *
 import pygame
-
+PANEL = ALTO//4
 pygame.init()
-pantalla = pygame.display.set_mode(RESOLUCION)
-pygame.draw.rect(pantalla,COLOR_BG,(00,00,ANCHO,ALTO))
-    
+Pantalla = pygame.display.set_mode(RESOLUCION)
+pygame.draw.rect(Pantalla,COLOR_BG,(00,00,ANCHO,ALTO))
+pygame.draw.rect(Pantalla,COLOR_PANEL,(00,00,ANCHO,ALTO/4))
+for ancho in range(0,ANCHO,ANCHO_CEL):
+    pygame.draw.line(Pantalla,GRIS,(ancho,PANEL),(ancho,ALTO),1)
+for alto in range(PANEL,(ALTO+1),ALTO_CEL):
+    pygame.draw.line(Pantalla,GRIS,(0,alto),(ANCHO,alto),1)    
 
 
 pygame.display.update()
+
+
+
 
 
 
--- seting.py
+++ seting.py
@@ -1,11 +1,16 @@
 import numpy as np
 from pygame import *
 
-ANCHO = 250
+ANCHO = 400
 ALTO = 500
-COLOR_BG = (50,50,50)
+COLOR_BG = (80,80,80)
+COLOR_PANEL = (50,60,50)
+PANEL = ALTO//4
+GRIS = (40,40,40)
 RESOLUCION = (ANCHO,ALTO)
-CANT_FIL = 15
-CANT_COL = 10
+ANCHO_CEL = 25
+ALTO_CEL = 25
+CANT_FIL = int(ALTO/ALTO_CEL)
+CANT_COL = int(ANCHO/ANCHO_CEL)
 matriz = np.zeros((CANT_FIL,CANT_COL),dtype = int)
 
