925c501167146508db916849b9e5992d6683c7c8
leogomez49 <jonasgomez08@gmail.com>
2023-10-18 15:25:21 -0300

    Update buscaminas.py

--- buscaminas.py
+++ buscaminas.py
@@ -1,10 +1,10 @@
 from seting import *
 import pygame
-PANEL = ALTO//4
+
 pygame.init()
 Pantalla = pygame.display.set_mode(RESOLUCION)
 pygame.draw.rect(Pantalla,COLOR_BG,(00,00,ANCHO,ALTO))
-pygame.draw.rect(Pantalla,COLOR_PANEL,(00,00,ANCHO,ALTO/4))
+pygame.draw.rect(Pantalla,COLOR_PANEL,(00,00,ANCHO,PANEL))
 for ancho in range(0,ANCHO,ANCHO_CEL):
     pygame.draw.line(Pantalla,GRIS,(ancho,PANEL),(ancho,ALTO),1)
 for alto in range(PANEL,(ALTO+1),ALTO_CEL):
