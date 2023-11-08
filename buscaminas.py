8caf8c78303689320e64669cf2a06443cf6f1341
leogomez49 <jonasgomez08@gmail.com>
2023-11-08 16:27:49 -0300

    upgrade 8/11/23

--- buscaminas.py
+++ buscaminas.py
@@ -5,17 +5,36 @@ import math
 
 pygame.init()
 Pantalla = pygame.display.set_mode(RESOLUCION)
-pygame.draw.rect(Pantalla,COLOR_BG,(00,00,ANCHO,ALTO))
+pygame.draw.rect(Pantalla,COLOR_BG,(00,00,ANCHO,PANEL_SUMA))
 pygame.draw.rect(Pantalla,COLOR_PANEL,(00,00,ANCHO,PANEL))
-for ancho in range(0,ANCHO,ANCHO_CEL):
-    pygame.draw.line(Pantalla,GRIS,(ancho,PANEL),(ancho,ALTO),1)
-for alto in range(PANEL,(ALTO+1),ALTO_CEL):
+for ancho in range(0,PANEL_SUMA,ANCHO_CEL):
+    pygame.draw.line(Pantalla,GRIS,(ancho,PANEL),(ancho,PANEL_SUMA),1)
+for alto in range(PANEL,(PANEL_SUMA+1),ALTO_CEL):
     pygame.draw.line(Pantalla,GRIS,(0,alto),(ANCHO,alto),1)    
 
 
 pygame.display.update()
 
+def sin_minas(X,Y):
+    print(X,Y)
+    for jup in range(X-1,X+2):
+        for up in range(Y-1,Y+2):
+          if jup < CANT_COL+1 or up < CANT_FIL+1:
+              if jup > 0 or up > 0:
+                  
+                  if matriz[up,jup] == 0 :
+                      print(up,jup)
+                      #dibujar_cel(Y,X)
+                      matriz[up,jup] = 8
+                      #sin_minas(X,Y)
+                      print(matriz)
+     
+    
+    
 
+
+
+    
 def aleatoriedad():
     cord_x = random.randint(0+1,CANT_COL-2)
     print(CANT_COL)
@@ -88,9 +107,9 @@ while True :
           print(event.pos[1])
           coord_fil = int(math.floor((event.pos[1])/ALTO_CEL)) 
           
-          
-          dibujar_cel(coord_fil,coord_col)
-          
+          if event.pos[1] > PANEL:
+              dibujar_cel(coord_fil,coord_col)
+          sin_minas(coord_col+1,coord_fil+1)
           pygame.display.update()
           #print(MATRIZ)
                       
